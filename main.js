06794899f428eaa6e6d244f34712e88d34791427
samarth2010 <76905546+samarth2010@users.noreply.github.com>
2021-04-28 13:48:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg", 
+"https://i.postimg.cc/wjMnFtMX/father.jpg",
+"https://i.postimg.cc/bw5W5zSK/mother.jpg",
+"samarth.jpg"];
+
+var names = ["Family Book", "Nagapradip", "Shantha", "Samarth"];
+var i = 0;
+
+function update123() {
+    i++;
+    var numbers_of_family_member_in_array = 3
+    if (i > numbers_of_family_member_in_array) {
+        i = 0;
+    }
+    updatedImage = images[i];
+    updatedName = names[i];
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
